485d7d6d0c6ae38d6bbe759aac5f0b6f52daa629
Goutham Reddy <gouthamvreddy@gmail.com>
2016-02-10 13:22:21 -0800

    linked chart to obesity data

--- scripts/bar_chart.js
+++ scripts/bar_chart.js
@@ -1,27 +1,37 @@
-var data = [4, 8, 15, 16, 23, 42];
-
 var width = 420,
-    barHeight = 20;
+  barHeight = 20;
 
 var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+  .range([0, width]);
 
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
+  .attr("width", width);
 
-var bar = chart.selectAll("g")
+
+d3.csv('data/overweight_children.csv', type, function(error, data){
+  console.log(data);
+  x.domain([0, d3.max(data, function(d) { return d.mean; })]);
+
+  chart.attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
     .data(data)
-  .enter().append("g")
+    .enter().append("g")
     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-bar.append("rect")
-    .attr("width", x)
+  bar.append("rect")
+    .attr("width", function(d) { return x(d.mean); })
     .attr("height", barHeight - 1);
 
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
+  bar.append("text")
+    .attr("x", function(d) { return x(d.mean) - 3; })
     .attr("y", barHeight / 2)
     .attr("dy", ".35em")
-    .text(function(d) { return d; });
+    .text(function(d) { return d.mean; });
+
+});
+
+function type(d) {
+  d.mean = +d.mean; // coerce to number
+  return d;
+}
